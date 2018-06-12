96d18574ca08107ae5ae8207c1579248f5415aaa
brinzi <bbrinzoiu@gmail.com>
2018-06-13 00:44:33 +0300

    Created v2 branch

--- Cryptography/src/main/java/Main.java
+++ Cryptography/src/main/java/Main.java
@@ -1,23 +1,23 @@
 package main.java;
 
-import main.java.crypto.Crypto;
+import main.java.fx.UiMain;
 
 public class Main{
 	
 	public static void main(String[] args) {
 
-//		UiMain.start(args);
-		try {
-			Crypto c = new Crypto("123465");
-			c.generateKeys("public", "private");
-			c.signDocument("private", "file", "singiature");
-		
-			c.encryptFile("file", "encypted");
-			c.decryptFile("encypted", "decrypted");
-			System.out.println(c.verifySigniature("public", "file", "singiature"));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		UiMain.start(args);
+//		try {
+//			Secure c = new Messages("123465");
+//			c.generateKeys("public", "private");
+//			c.sign("private", "file", "singiature");
+//		
+////			c.performEncrypt("file", "encypted");
+////			c.performDecrypt(document, out);("encypted", "decrypted");
+//			System.out.println(c.verify("public", "file", "singiature"));
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 }
--- Cryptography/src/main/java/fx/KeysWindowController.java
+++ Cryptography/src/main/java/fx/KeysWindowController.java
@@ -1,4 +1,4 @@
-package main.java.ui;
+package main.java.fx;
 
 public class KeysWindowController {
 
