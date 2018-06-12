c76998bd342f7c84272b237d2701aa1af583203f
RADUFURNEA <radufurnea@gmail.com>
2018-06-12 22:05:22 +0300

    imlememnted part of UI

---
+++ Cryptography/src/main/java/ui/MainWindowController.java
@@ -0,0 +1,88 @@
+package main.java.ui;
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+
+public class MainWindowController {
+	
+	private static Stage fileStage;
+	
+	private static AnchorPane keysLayout;
+	private static Stage keysStage;
+	
+	public void showKeysStage() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("../../resources/KeysWindow.fxml"));
+		keysLayout = loader.load();
+		keysStage = new Stage();
+		keysStage.setTitle("Select Paths for Keys");
+		keysStage.initModality(Modality.WINDOW_MODAL);
+		Scene scene = new Scene(keysLayout,302,177);
+		
+		keysStage.setScene(scene);
+		keysStage.show();
+	}
+
+
+	@FXML private Button loadFile;
+	
+	@FXML private Button createKey;
+	
+	@FXML private Button generateKey;
+	
+	@FXML private Button sign;
+	
+	@FXML private Button verify;
+	
+	@FXML private TextField filePath;
+	
+	@FXML private Label actions;
+	
+	@FXML
+	public void onButtonClick(ActionEvent event) throws IOException{
+		if(event.getSource()==loadFile) {
+			start(fileStage);
+		}
+		if(event.getSource()==generateKey) {
+			showKeysStage();
+		}
+	}
+	
+	private void setPath(String string) {
+		filePath.setText(string);
+	}
+	
+
+	public void start(final Stage stage) throws IOException {
+		final FileChooser fileChooser = new FileChooser();
+		File file = fileChooser.showOpenDialog(stage);
+		if (file != null) {
+			setPath(file.getPath());
+		}
+		else {
+			Alert alert = new Alert(AlertType.WARNING);
+			alert.setTitle("No file!");
+			alert.setHeaderText("No file selected!");
+			alert.setContentText("Please select a valid file.");
+			alert.showAndWait();
+		}
+		
+	}
+
+	
+}
--- Cryptography/src/main/java/ui/UiMain.java
+++ Cryptography/src/main/java/ui/UiMain.java
@@ -5,36 +5,56 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
-import main.java.Main;
+
 
 public class UiMain extends Application {
+	
+	public static String password;
+	
 	private static Stage primaryStage;
-	private static BorderPane mainLayout;
+	private static Stage entryPassStage;
+
+	private static AnchorPane mainLayout;
+	private static AnchorPane entryPassLayout;
+	
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		UiMain.primaryStage = primaryStage;
 		UiMain.primaryStage.setTitle("Cryptography App");
 		showMainView();
+		showEntryPassStage();
 	}
 
 	private void showMainView() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(getClass().getResource("../../resources/MainWindow.fxml"));
 		mainLayout = loader.load();
-		Scene scene = new Scene (mainLayout,500,500);
+		Scene scene = new Scene (mainLayout,290,305);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
-	private void showLogin() throws IOException{
+	private void showEntryPassStage() throws IOException {
 		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("login/Login.fxml"));
-		BorderPane login = new BorderPane();
-		mainLayout.setCenter(login);
+		loader.setLocation(getClass().getResource("../../resources/PassEntryWindow.fxml"));
+		entryPassLayout = loader.load();
+		entryPassStage = new Stage();
+		entryPassStage.setTitle("Insert Password");
+		entryPassStage.initModality(Modality.WINDOW_MODAL);
+		entryPassStage.initOwner(primaryStage);
+		Scene scene = new Scene(entryPassLayout,275,91);
+		
+		entryPassStage.setScene(scene);
+		entryPassStage.show();
 	}
+	
+
+	
+	
 
 	public static void start(String[] args) {
 		launch(args);
