ec8f37e5ea91bfaa3666cf4f4022419344618e0d
brinzi <bbrinzoiu@gmail.com>
2018-06-12 18:36:44 +0300

    ade crypto mock

--- Cryptography/src/main/java/crypto/Crypto.java
+++ Cryptography/src/main/java/crypto/Crypto.java
@@ -2,4 +2,16 @@ package main.java.crypto;
 
 public class Crypto {
 
+	public void generateKeys(String password, String filePath) {
+		System.out.println("Genrating..");
+	}
+	
+	public void signDocument(String keyPath, String filePath) {
+		System.out.println("Document was signed");
+	}
+	
+	public void verifySigniature(String keyPath, String filePath) {
+		System.out.println("Checking keys...");
+	}
+	
 }
