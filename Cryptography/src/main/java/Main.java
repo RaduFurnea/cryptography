100e785082b136134ef787132aad4fdf41c29e12
brinzi <bbrinzoiu@gmail.com>
2018-06-12 23:47:17 +0300

    crypto now working

--- Cryptography/src/main/java/Main.java
+++ Cryptography/src/main/java/Main.java
@@ -1,10 +1,20 @@
 package main.java;
 
-import main.java.ui.UiMain;
+import main.java.crypto.Crypto;
 
 public class Main{
 	
 	public static void main(String[] args) {
-		UiMain.start(args);
+
+		
+		try {
+			Crypto c = new Crypto("123465");
+			c.generateKeys("public", "private");
+			c.signDocument("private", "file.txt", "signed");
+			System.out.println(c.verifySigniature("public", "file.txt", "singiature"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- Cryptography/src/main/java/crypto/Crypto.java
+++ Cryptography/src/main/java/crypto/Crypto.java
@@ -13,47 +13,102 @@ import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Crypto {
 	private KeyPairGenerator keyGen;
 
+	private String password;
+
+	public Crypto(String password) throws Exception {
+		this.password = password;
+	}
+
 	private Key readKeyFromFs(String keyPath, boolean isPrivate) throws Exception {
 		Key pk = null;
-		File f = new File(keyPath);
+		System.out.println(keyPath);
+		byte[] key;
+		EncodedKeySpec keySpec;
+		if (isPrivate) {
+			key = decrypt(getFileAsBytes(keyPath));
+			keySpec = new PKCS8EncodedKeySpec(key);
+		} else {
+			key = getFileAsBytes(keyPath);
+			keySpec = new X509EncodedKeySpec(key);
+		}
+
+		KeyFactory kf = KeyFactory.getInstance("DSA");
+
+		if (isPrivate)
+			pk = kf.generatePrivate(keySpec);
+		else
+			pk = kf.generatePublic(keySpec);
+
+		return pk;
+	}
+
+	private byte[] getFileAsBytes(String path) throws IOException {
+		File f = new File(path);
 		FileInputStream fis = new FileInputStream(f);
 		DataInputStream dis = new DataInputStream(fis);
 		byte[] keyBytes = new byte[(int) f.length()];
 		dis.readFully(keyBytes);
 		dis.close();
 
-		X509EncodedKeySpec spec = new X509EncodedKeySpec(keyBytes);
-		KeyFactory kf = KeyFactory.getInstance("DSA");
-		if(isPrivate)
-			pk = kf.generatePrivate(spec);
-		else 
-			pk = kf.generatePublic(spec);
-		
-		return pk;
-	}
-	
-	private byte[] readSigniature(String filePath) throws IOException {
-		FileInputStream sigfis = new FileInputStream(filePath);
-		byte[] sigToVerify = new byte[sigfis.available()]; 
-		sigfis.read(sigToVerify);
-		sigfis.close();
-		
-		return sigToVerify;	
-	}
-	
-	private void writeBytesToDisk(byte[] arr) throws Exception {
-		try (FileOutputStream fos = new FileOutputStream("pathname")) {
-			   fos.write(arr);
-			}
+		return keyBytes;
 	}
 
-	public void generateKeys(String password, String publicKeyPath, String privateKeyPath)
-			throws NoSuchAlgorithmException {
+	private void writeBytesToDisk(byte[] arr, String path) throws Exception {
+		try (FileOutputStream fos = new FileOutputStream(path)) {
+			fos.write(arr);
+		}
+	}
+
+	private SecretKey deriveAesKey() throws NoSuchAlgorithmException, InvalidKeySpecException {
+		SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+		KeySpec spec = new PBEKeySpec(password.toCharArray(), "salt".getBytes(), 65536, 256);
+		SecretKey tmp = factory.generateSecret(spec);
+		return new SecretKeySpec(tmp.getEncoded(), "AES");
+	}
+
+	private byte[] readSigniature(String filePath) throws IOException {
+		FileInputStream sigfis = new FileInputStream(filePath);
+		byte[] sigToVerify = new byte[sigfis.available()];
+		sigfis.read(sigToVerify);
+		sigfis.close();
+
+		return sigToVerify;
+	}
+
+	public byte[] encrypt(byte[] bytesToEncypt) throws Exception {
+		Key aesKey = deriveAesKey();
+		Cipher cipher = Cipher.getInstance("AES");
+
+		cipher.init(Cipher.ENCRYPT_MODE, aesKey);
+
+		return cipher.doFinal(bytesToEncypt);
+	}
+
+	public byte[] decrypt(byte[] bytesToDecrypt) throws Exception {
+		Key aesKey = deriveAesKey();
+		Cipher cipher = Cipher.getInstance("AES");
+
+		cipher.init(Cipher.DECRYPT_MODE, aesKey);
+
+		return cipher.doFinal(bytesToDecrypt);
+	}
+
+	public void generateKeys(String publicKeyPath, String privateKeyPath) throws Exception {
 		this.keyGen = KeyPairGenerator.getInstance("DSA");
 		keyGen.initialize(2048);
 
@@ -61,73 +116,40 @@ public class Crypto {
 		byte[] publicKeyBytes = keys.getPublic().getEncoded();
 		byte[] privateKeyByte = keys.getPrivate().getEncoded();
 
-		FileOutputStream fos;
-		try {
-			fos = new FileOutputStream(publicKeyPath);
-			fos.write(publicKeyBytes);
-			fos.close();
-
-			fos = new FileOutputStream(privateKeyPath);
-			fos.write(privateKeyByte);
-			fos.close();
-		} catch (IOException e) {
-			System.err.println("Error saveing keys");
-		}
+		writeBytesToDisk(publicKeyBytes, publicKeyPath);
+		writeBytesToDisk(encrypt(privateKeyByte), privateKeyPath);
 	}
 
-	public void signDocument(String privateKeyPath, String filePath) throws Exception {
+	public void signDocument(String privateKeyPath, String filePath, String pathToSigniature) throws Exception {
 
 		PrivateKey key = (PrivateKey) readKeyFromFs(privateKeyPath, true);
-
 		Signature dsa = Signature.getInstance("SHA256withDSA");
 
 		/* Initializing the object with a private key */
 		dsa.initSign(key);
-		byte[] fileBytes = null;
-		try {
-			// read file as byte array
-			File f = new File(filePath);
-			FileInputStream fis = new FileInputStream(f);
-			DataInputStream dis = new DataInputStream(fis);
-			fileBytes = new byte[(int) f.length()];
-			dis.readFully(fileBytes);
-			dis.close();
-		} catch (Exception e) {
-			throw new Exception("Error reading file " + e );
-		}
+		byte[] fileBytes = getFileAsBytes(filePath);
 
 		/* Update and sign the data */
 		dsa.update(fileBytes);
-		this.writeBytesToDisk(dsa.sign());
+		this.writeBytesToDisk(dsa.sign(), "singiature");
 	}
 
-	public boolean verifySigniature(String keyPath, String filePath) throws Exception {
+	public boolean verifySigniature(String keyPath, String filePath, String pathToSigniature) throws Exception {
 		Signature dsa = Signature.getInstance("SHA256withDSA");
 
 		PublicKey key = (PublicKey) readKeyFromFs(keyPath, false);
-		
+
 		/* Initializing the object with a public key */
 		dsa.initVerify(key);
-		
-		byte[] sigToVerify = this.readSigniature(filePath);
-		
-		byte[] fileBytes = null;
-		try {
-			// read file as byte array
-			File f = new File(filePath);
-			FileInputStream fis = new FileInputStream(f);
-			DataInputStream dis = new DataInputStream(fis);
-			fileBytes = new byte[(int) f.length()];
-			dis.readFully(fileBytes);
-			dis.close();
-		} catch (Exception e) {
-			throw new Exception("Error reading file " + e );
-		}
+
+		byte[] sigToVerify = this.readSigniature(pathToSigniature);
+
+		byte[] fileBytes = getFileAsBytes(filePath);
 
 		dsa.update(fileBytes);
-		
+
 		return dsa.verify(sigToVerify);
-	
+
 	}
 
 }
