fb320bd821b5ebb0c9cc7cedc44aa49c50ee0d04
brinzi <bbrinzoiu@gmail.com>
2018-06-12 18:23:22 +0300

    adding new files

--- Cryptography/src/main/java/Main.java
+++ Cryptography/src/main/java/Main.java
@@ -1,44 +1,10 @@
-package main;
+package main.java;
 
-import java.io.IOException;
+import main.java.ui.UiMain;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-
-public class Main extends Application{
-
-	private static Stage primaryStage;
-	private static BorderPane mainLayout;
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		Main.primaryStage = primaryStage;
-		Main.primaryStage.setTitle("Cryptography App");
-		showMainView();
-		showLogin();
-	}
-
-	private void showMainView() throws IOException {
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("view/MainView.fxml"));
-		mainLayout = loader.load();
-		Scene scene = new Scene (mainLayout,500,500);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
-	
-	private void showLogin() throws IOException{
-		FXMLLoader loader = new FXMLLoader();
-		loader.setLocation(Main.class.getResource("login/Login.fxml"));
-		BorderPane login = new BorderPane();
-		mainLayout.setCenter(login);
-	}
-	
+public class Main{
 	
 	public static void main(String[] args) {
-		launch(args);
+		UiMain.start(args);
 	}
 }
---
+++ Cryptography/src/main/java/ui/UiMain.java
@@ -0,0 +1,44 @@
+package main.java.ui;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import main.java.Main;
+
+public class UiMain  extends Application {
+	private static Stage primaryStage;
+	private static BorderPane mainLayout;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		UiMain.primaryStage = primaryStage;
+		UiMain.primaryStage.setTitle("Cryptography App");
+		showMainView();
+		showLogin();
+	}
+
+	private void showMainView() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("view/MainView.fxml"));
+		mainLayout = loader.load();
+		Scene scene = new Scene (mainLayout,500,500);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	private void showLogin() throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("login/Login.fxml"));
+		BorderPane login = new BorderPane();
+		mainLayout.setCenter(login);
+	}
+
+	public static void start(String[] args) {
+		launch(args);
+		
+	}
+}
